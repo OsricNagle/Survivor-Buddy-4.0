6de3ba382b72ec54600fea222bd8d132557c4657
khutes <kyle.hutto3@gmail.com>
2020-11-22 22:18:12 -0600

    Added doc strings to BuddyMessageClient

--- gui/BuddyMessageClient.py
+++ gui/BuddyMessageClient.py
@@ -1,11 +1,25 @@
 import socket
 import threading
-from tkinter import *
-from tkinter.ttk import *
 
 class BuddyMessageClient:
+    """
+    A TCP client which sends messages in text form. Each message sent connects, sends, 
+    then disconnects. 
+
+    :param server_ip: the ip address of the server
+    :type server_ip: String
+    :param port_num: the port number of the server
+    :type port_num: int
+    :param frame: the frame on which error messages will be displayed. Defaults to None
+    :type frame: Tkinter frame, optional
+    :param str_format: the encoding format in which messages are sent
+    :type str_format: String
+    """
 
     def __init__(self, server_ip, port_num, frame=None, str_format='utf-8'):
+        """
+        Constructor for BuddyMessageClient
+        """
 
         self.server_ip = server_ip
         self.port_num = port_num
@@ -15,13 +29,36 @@ class BuddyMessageClient:
         self.app_frame = frame
 
     def show_error(self, error_msg):
+        """
+        Displays the error message on the frame
+
+        :param error_msg: the content of the error message
+        :type error_msg: String
+        """
         self.app_frame.showPopupMessage(msg_text=error_msg)
 
     def setPortNum(self, new_port):
+        """
+        Sets the port number
+
+        :param new_port: the value of the new port number
+        :type new_port: int
+        """
         self.port_num = new_port
         self.full_addr = (self.server_ip, self.port_num)
 
     def connect(self, text="DEFAULT_MESSAGE"):
+        """
+        Connects to the server. Displays error if unsuccessful
+
+        :param text: unused
+        :type text: String
+        :return: true if connection is successful, false if there is a connection error,
+            and None if undefined error occurrs
+        :rtype: boolean
+        """
+
+
         error_msg = "Message Connection Error:\n"
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -55,14 +92,29 @@ class BuddyMessageClient:
         return None
 
     def disconnect(self):
+        """
+        Disconnects from the server if connected, otherwise does nothing
+        """
         if(self.client_socket is not None):
             self.client_socket.close()
             self.client_socket = None
 
     def sendMsg(self, msg_str):
+        """
+        Starts handleSend in a new thread which sends a message to the server
+
+        :param msg_str: the message to send to the server
+        :type msg_str: String
+        """
         threading.Thread(target=self.handleSend, args=(msg_str,)).start()
 
     def handleSend(self, msg_str):
+        """
+        Connects to the server, sends a message, then disconnects
+
+        :param msg_str: the message to send to the server
+        :type msg_str: String
+        """
         if self.connect(msg_str):
             self.client_socket.sendall(msg_str.encode(self.str_format))
             self.disconnect()
