d64282017a21f600dc4d5128004038044e13f903
khutes <kyle.hutto3@gmail.com>
2020-11-23 14:25:45 -0600

    Removed guiscrcpy as it is no longer needed

--- guiscrcpy/constants.py
+++
@@ -1,8 +0,0 @@
-FONTS = [
-    'NotoSans-BoldItalic.ttf',
-    'NotoSans-Italic.ttf',
-    'TitilliumWeb-Bold.ttf',
-    'NotoSans-Bold.ttf',
-    'NotoSans-Regular.ttf',
-    'TitilliumWeb-Regular.ttf',
-]
