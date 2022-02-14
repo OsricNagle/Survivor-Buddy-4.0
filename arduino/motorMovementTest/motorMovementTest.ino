808a25c91263ab65315928e2c42675359088d7af
Osric Nagle <osric.nagle@gmail.com>
2022-02-14 17:30:08 -0600

    Create motorMovementTest.ino
    Testing function for changes to VarSpeedServo.cpp

---
+++ arduino/motorMovementTest/motorMovementTest.ino
@@ -0,0 +1,40 @@
+#include "C:\Users\osric\Documents\GitHub\Survivor-Buddy-4.0\arduino\motorMovement\VarSpeedServo.cpp"
+
+int servo = 5;
+int feedback = A0;
+int base_1 = 3;
+int base_2 = 4;
+
+VarSpeedServo test;
+VarSpeedServo base1;
+VarSpeedServo base2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+
+  pinMode(servo, OUTPUT);
+  pinMode(feedback, INPUT);
+  pinMode(base_1, OUTPUT);
+  pinMode(base_2, OUTPUT);
+
+  test.attach(servo);
+  base1.attach(base_1);
+  base2.attach(base_2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  test.write(0);
+//  bool impaired = test.wait(feedback);
+//  Serial.println("Impaired = " + String(impaired));
+//  test.wait();
+//  test.write(90);
+//  impaired = test.wait(feedback);
+//  Serial.println("Impaired = " + String(impaired));
+//  test.wait();
+
+  Serial.println("Position: " + String(analogRead(feedback)));
+  delay(1000);
+}
